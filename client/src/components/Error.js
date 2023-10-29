dbbcadee15fb66b445999ac12366eaeee60281dd
sahilhedau49 <hedausahil49@gmail.com>
2023-10-29 21:09:00 +0530

    Error Page Added

--- client/src/components/Error.js
+++ client/src/components/Error.js
@@ -1,7 +1,17 @@
 import React from "react";
+import errorImg from "../assets/images/error.jpg";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div className="block w-fit mx-auto my-6">
+      <div className="text-center">
+        <p className="uppercase overflow-y-hidden mb-4 text-3xl font-semibold text-slate-600">
+          Page not found
+        </p>
+        <img className="error-img" src={errorImg} alt="Error Message" />
+      </div>
+    </div>
+  );
 };
 
 export default Error;
