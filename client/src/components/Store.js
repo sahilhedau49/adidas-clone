b2dcffc121c0d897b665f89b5ab2e1656352206e
sahilhedau49 <hedausahil49@gmail.com>
2023-11-13 22:30:47 +0530

    Removed Bugs

--- client/src/components/Store.js
+++ client/src/components/Store.js
@@ -6,27 +6,41 @@ import ProductCard from "./ProductCard";
 const Store = ({ category }) => {
   const [data, setData] = useState([]);
 
-  const getData = async (flag) => {
-    let querry = flag;
-    if (querry === "all") {
-      const dataRsv = await axios.get(`http://localhost:8000/products/all`);
-      setData(dataRsv.data);
-      // console.log(dataRsv.data);
-    } else {
-      const dataRsv = await axios.get(
-        `http://localhost:8000/products?category=${querry}`
-      );
-      setData(dataRsv.data);
-    }
-    // console.log(data);
+  const handleSearchChange = (e) => {
+    var search = e.target.value;
+    console.log(search);
   };
 
   useEffect(() => {
+    const getData = async (flag) => {
+      let querry = flag;
+      if (querry === "all") {
+        const dataRsv = await axios.get(`http://localhost:8000/products/all`);
+        setData(dataRsv.data);
+        // console.log(dataRsv.data);
+      } else {
+        const dataRsv = await axios.get(
+          `http://localhost:8000/products?category=${querry}`
+        );
+        setData(dataRsv.data);
+      }
+      // console.log(data);
+    };
+
     getData(category);
   }, [category]);
 
   return (
-    <div className="bg-gray-100 pb-20">
+    <div className="relative bg-gray-100 pb-20">
+      <div className="absolute right-6 top-4 text-left">
+        <p className="text-xl pb-2">Search: </p>
+        <input
+          onChange={handleSearchChange}
+          className="px-4 py-2 outline-none rounded-xl"
+          type="text"
+          name="search"
+        />
+      </div>
       <div className="my-20">
         <Substores category={category} />
       </div>
