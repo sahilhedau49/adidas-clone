45d8562181e47bdec1943d5ddcf9eb9e7f320d7d
sahilhedau49 <hedausahil49@gmail.com>
2024-08-18 21:49:26 +0530

    Bug Fixing

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,37 @@ const app = express();
 const endpointSecret = process.env.STRIPE_WEBHOOK_ENDPOINT_SECRET;
 
 app.post(
+  "/webhookWithData",
+  express.raw({ type: "application/json" }),
+  async (request, response) => {
+    const sig = request.headers["stripe-signature"];
+
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+      console.log("Webhook Verified");
+    } catch (err) {
+      console.log(`Webhook Error: ${err.message}`);
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    // Handle the event
+    switch (event.type) {
+      case "checkout.session.completed":
+        const session = event.data.object;
+        console.log("Session:", session);
+        break;
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    response.sendStatus(200);
+  }
+);
+
+app.post(
   "/webhook",
   express.raw({ type: "application/json" }),
   async (request, response) => {
