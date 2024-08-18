0e815ef55ea41b7bc814a237eed3c2d92b2dcaf9
sahilhedau49 <hedausahil49@gmail.com>
2024-08-18 20:12:14 +0530

    Bug Fixed

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.post(
       event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
       console.log("Webhook Verified");
     } catch (err) {
-      console.log(err);
+      console.log(`Webhook Error: ${err.message}`);
       response.status(400).send(`Webhook Error: ${err.message}`);
       return;
     }
@@ -34,8 +34,6 @@ app.post(
       case "payment_intent.succeeded":
         const paymentIntent = event.data.object;
         console.log("Payment Intent --> ", paymentIntent);
-        const metadata = paymentIntent.metadata;
-        console.log("Metadata --> ", metadata);
         break;
       default:
         console.log(`Unhandled event type ${event.type}`);
@@ -120,19 +118,19 @@ app.put("/productUpdate/:id", async (req, res) => {
 
 // Checkout api
 app.post("/create-checkout-session", async (req, res) => {
-  const { products, addressData, userData } = req.body;
+  // const { products, addressData, userData } = req.body;
 
-  const productsData = products.map((prod) => ({
-    productDetails: prod._id,
-    quantity: prod.quantity,
-    totalPrice: prod.totalPrice,
-  }));
+  // const productsData = products.map((prod) => ({
+  //   productDetails: prod._id,
+  //   quantity: prod.quantity,
+  //   totalPrice: prod.totalPrice,
+  // }));
 
-  const orderData = {
-    addressData: addressData,
-    productsData: productsData,
-    userData: userData,
-  };
+  // const orderData = {
+  //   addressData: addressData,
+  //   productsData: productsData,
+  //   userData: userData,
+  // };
 
   // try {
   //   await Order.insertMany([orderData]);
@@ -140,6 +138,8 @@ app.post("/create-checkout-session", async (req, res) => {
   //   res.json({ error: error });
   //   return;
   // }
+
+  const { products } = req.body;
 
   const lineItems = products.map((prod) => ({
     price_data: {
@@ -159,7 +159,6 @@ app.post("/create-checkout-session", async (req, res) => {
     shipping_address_collection: {
       allowed_countries: ["IN"],
     },
-    metadata: JSON.stringify(orderData),
     success_url: `${process.env.DOMAIN_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
     cancel_url: `${process.env.DOMAIN_URL}/cancel`,
   });
